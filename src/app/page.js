a25b7e22123ea62cc6008f99b6833e97d164f61b
seungjoo234 <seungjoo234@naver.com>
2024-10-16 14:11:56 +0900

    2번째 커밋

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,9 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <h1>임승주 202030229 Home</h1>;
+  return (
+    <>
+      <h1>임승주 202030229 Home</h1>
+    </>
+  );
 }
